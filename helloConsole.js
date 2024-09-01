74ed943f7ebbe06c3544bfb9ad00d3c1fe6105de
Abdul-Samad-Furkhanullah-Mohammed <abdulsamadfurkhanullah1998@gmail.com>
2024-09-01 02:21:44 -0700

    Update helloConsole.js

--- helloConsole.js
+++ helloConsole.js
@@ -1,5 +1,12 @@
-// Unique variable for message
+// variable for message
 let consoleMessage = 'Hello, World!';
+
+//variable for name
+let name = 'Abdul Samad Furkhanullah Mohammed'
 
 // Log the message to the console
 console.log(consoleMessage);
+
+// Log the message to the console
+console.log(name);
+
